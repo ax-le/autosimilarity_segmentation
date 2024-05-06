7d3077cafbac9c7c4956d86676c0dc7a7be0f94d
Axel Marmoret <axel.marmoret@imt-atlantique.fr>
2024-05-06 17:12:41 +0200

    The init also

--- as_seg/__init__.py
+++ as_seg/__init__.py
@@ -2,9 +2,9 @@ from . import autosimilarity_computation
 from . import barwise_input
 from . import data_manipulation
 from . import CBM_algorithm
-from . import foote_novelty
+#from . import foote_novelty
 
 from .model import current_plot
 from .model import errors
-from .model import features
+from .model import signal_to_spectrogram
 from .model import display_results
